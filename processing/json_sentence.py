a62a6fc6bdf7e44d7d892e59f684310036f4eb8f
Zhiqi Huang <Charles.hzq@gmail.com>
2019-04-10 01:17:49 -0400

    concat sentence of all wiki doc

---
+++ processing/json_sentence.py
@@ -0,0 +1,53 @@
+import json, sys, os
+import argparse
+import threading
+
+
+def merge_task(pid, task_list, args):
+	context = []
+	outputname = 'CONCAT_{}'.format(pid) 
+	for folder in task_list:
+		working_dir = '{}/{}'.format(args.input_dir,folder)
+		for fname in os.listdir(working_dir):
+			with open('{}/{}'.format(working_dir,fname), 'r') as f:
+				raw = f.readlines()
+			f.close()
+			for item in raw:
+				item_dict = json.loads(item)
+				paragraph = parse(item_dict['text'])
+				context.append(paragraph)
+	with open('{}/{}'.format(args.output_dir, outputname), "w+") as f:
+		f.write('\n'.join(context))
+	f.close()
+
+def split(a, n):
+	k, m = divmod(len(a), n)
+	return (a[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(n))
+
+def parse(text):
+	sentences = text.split('\n')
+	#remove invalid sentences
+	sentences = [line for line in sentences if len(line) >= 20]
+	return '\n'.join(sentences)
+
+
+def main():
+	parser = argparse.ArgumentParser(description="Merge json in to corpus")
+	parser.add_argument('--input_dir', type=str, default='', help='dump file directory')
+	parser.add_argument('--output_dir', type=str, default='', help='output directory')
+	parser.add_argument('--num_process', type=int, default=2, help='number of parallel')
+	
+	args = parser.parse_args()
+
+	dump_dir = os.listdir(args.input_dir)
+	tasks = list(split(dump_dir, args.num_process))
+
+	threads = []
+	for i in range(args.num_process):
+		t = threading.Thread(target=merge_task, args=(i, tasks[i], args,))
+		threads.append(t)
+		t.start()
+
+if __name__ == '__main__':
+	main()
+	
