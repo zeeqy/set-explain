8136c227170aa3c4346fb3d5683ffbcfaaac13b1
Zhiqi Huang <Charles.hzq@gmail.com>
2019-05-15 19:13:37 -0400

    load sent test

--- processing/querying_test.py
+++ processing/querying_test.py
@@ -1,5 +1,6 @@
 import json, sys, os
 import argparse
+import time
 from querying_tools import matching_tools
 
 """
@@ -13,20 +14,27 @@ def main():
     parser.add_argument('--inverted_dir', type=str, default='', help='inverted document directory')
     parser.add_argument('--query_entity', type=str, default='', help='querying entity')
     parser.add_argument('--output_dir', type=str, default='', help='output directory')
-    parser.add_argument('--num_process', type=int, default=2, help='number of parallel')
     
     args = parser.parse_args()
 
     outputname = 'query_test_output.txt'
 
-    mtools = matching_tools(args.entity_dir, args.inverted_dir, args.sentence_dir, args.num_process)
+    start_time = time.time()
+    mtools = matching_tools(args.entity_dir, args.inverted_dir, args.sentence_dir)
 
-    print("initialization finished")
+    print("initialization finished, took {} seconds".format(time.time() - start_time))
     sys.stdout.flush()
 
+    start_time = time.time()
     mentioned_keys = mtools.entityMentioned(args.query_entity)
 
-    print("search for entity mention finished")
+    print("search for entity mention finished, took {} seconds".format(time.time() - start_time))
+    sys.stdout.flush()
+
+    start_time = time.time()
+    mtools.loadSent()
+
+    print("load sentence finished, took {} seconds".format(time.time() - start_time))
     sys.stdout.flush()
 
     content = mtools.key2Text(mentioned_keys)
