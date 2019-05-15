2a0c683fff4b8e1683a87f73a83b0ca38a399875
Zhiqi Huang <Charles.hzq@gmail.com>
2019-05-15 19:08:07 -0400

    query generate

--- processing/querying_tools.py
+++ processing/querying_tools.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
 import json, sys, os
-import threading
-import queue
 
 class matching_tools(object):
-	def __init__(self, entity_dir, inverted_dir, sentence_dir, num_process):
+	def __init__(self, entity_dir, inverted_dir, sentence_dir):
 		self.entity_dir = entity_dir
 		self.inverted_dir = inverted_dir
 		self.sentence_dir = sentence_dir
-		self.num_process = num_process
+		self.load_sent = False
+		self.sent_json = {}
 
 		with open('{}/INVERTED_INDEX.json'.format(self.inverted_dir), 'r') as f:
 			raw_index = f.read()
@@ -33,6 +32,13 @@ class matching_tools(object):
 
 		self.entityset = set(raw_list.split('\n'))
 
+	def loadSent(self):
+		if self.load_sent == False:
+			with open('{}/SENTENCE_ENTITY.txt'.format(self.sentence_dir, fname), 'r') as f:
+				for line in f:
+					self.sent_json.update(json.loads(line))
+			f.close()
+
 	def validEntity(self, entity):
 		return True if entity in self.entityset else False
 
@@ -46,48 +52,11 @@ class matching_tools(object):
 		else:
 			return False
 
-	def _split(self, a, n):
-		k, m = divmod(len(a), n)
-		return (a[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(n))
-
-	def storeInQueue(f):
-		def wrapper(*args):
-			self.sent_queue.put(f(*args))
-		return wrapper
-
-	@storeInQueue
-	def merge_task(self, task_list, mentionedSet):
-		content = []
-		for fname in task_list:
-
-			sent_json = {}
-			
-			with open('{}/{}'.format(self.sentence_dir, fname), 'r') as f:
-				for line in f:
-					sent_json.update(json.loads(line))
-			f.close()
-
-			key_set = set(sent_json.keys())
-			inter = mentionedSet.intersection(key_set)
-
-			for key in inter:
-				content.append(json.dumps({key:sent_json[key]}))
-		return content
-
 	def key2Text(self, mentionedKeys):
-		task_list = os.listdir(self.sentence_dir)
-
 		mentionedSet = set(mentionedKeys)
-
 		content = []
 
-		tasks = list(self._split(task_list, self.num_process))
-
-		self.sent_queue = queue.Queue()
-
-		for i in range(self.num_process):
-			t = threading.Thread(target=self.merge_task, args=(tasks[i], mentionedSet, ))
-			t.start()
-			content += self.sent_queue.get()
+		for key in mentionedSet:
+			content.append(json.dumps({key:self.sent_json[key]}))
 
 		return content
