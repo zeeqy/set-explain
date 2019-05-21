9654bcc07299c4700d3e8dfef16e1f27c73ca703
Zhiqi Huang <Charles.hzq@gmail.com>
2019-05-21 15:17:36 -0400

    querying tool

--- processing/querying_tools.py
+++ processing/querying_tools.py
@@ -3,62 +3,74 @@ import json, sys, os
 import pandas as pd
 
 class matching_tools(object):
-	def __init__(self, entity_dir, inverted_dir, sentence_dir):
-		self.entity_dir = entity_dir
-		self.inverted_dir = inverted_dir
-		self.sentence_dir = sentence_dir
-		self.load_sent = False
+    def __init__(self, entity_dir, inverted_dir, sentence_dir):
+        self.entity_dir = entity_dir
+        self.inverted_dir = inverted_dir
+        self.sentence_dir = sentence_dir
+        self.load_sent = False
 
-		self.inverted_index = pd.read_csv('{}/inverted_index.csv'.format(self.inverted_dir),
-								delimiter='\t', error_bad_lines=False,
-								header=None, names=['eid','mid'], dtype={'eid':str,'eid':str})
+        self.inverted_index = pd.read_csv('{}/inverted_index.csv'.format(self.inverted_dir),
+                                delimiter='\t', error_bad_lines=False,
+                                header=None, names=['eid','mid'], dtype={'eid':str,'eid':str})
 
-		with open('{}/entity2id.txt'.format(self.entity_dir), 'r') as f:
-			raw_entity2id = f.read()
-		f.close()
+        with open('{}/entity2id.txt'.format(self.entity_dir), 'r') as f:
+            raw_entity2id = f.read()
+        f.close()
 
-		self.entity2id = json.loads(raw_entity2id)
+        self.entity2id = json.loads(raw_entity2id)
 
-		with open('{}/id2entity.txt'.format(self.entity_dir), 'r') as f:
-			raw_id2entity = f.read()
-		f.close()
+        with open('{}/id2entity.txt'.format(self.entity_dir), 'r') as f:
+            raw_id2entity = f.read()
+        f.close()
 
-		self.id2entity = json.loads(raw_id2entity)
+        self.id2entity = json.loads(raw_id2entity)
 
-		with open('{}/entitylist.txt'.format(self.entity_dir), 'r') as f:
-			raw_list = f.read()
-		f.close()
+        with open('{}/entitylist.txt'.format(self.entity_dir), 'r') as f:
+            raw_list = f.read()
+        f.close()
 
-		self.entityset = set(raw_list.split('\n'))
+        self.entityset = set(raw_list.split('\n'))
 
-	def loadSent(self):
-		if self.load_sent == False:
+    def loadSent(self):
+        if self.load_sent == False:
 
-			self.sent_index = pd.read_csv('{}/sentence_entity.csv'.format(self.sentence_dir),
-								delimiter='|', error_bad_lines=False,
-								header=None, names=['mid','title','did', 'pid', 'sid', 'mentioned', 'text'],
-								dtype={'mid':str,'title':str,'did':str, 'pid':str, 'sid':str, 'mentioned':str, 'text':str})
-			self.load_sent = True
+            self.sent_index = pd.read_csv('{}/sentence_entity.csv'.format(self.sentence_dir),
+                                delimiter='|', error_bad_lines=False,
+                                header=None, names=['mid','title','did', 'pid', 'sid', 'mentioned', 'text'],
+                                dtype={'mid':str,'title':str,'did':str, 'pid':str, 'sid':str, 'mentioned':str, 'text':str})
+            self.load_sent = True
 
-	def validEntity(self, entity):
-		return True if entity in self.entityset else False
+    def validEntity(self, entity):
+        return True if entity in self.entityset else False
 
-	def eidFinder(self, entity):
-		return self.entity2id[entity]
+    def eidFinder(self, entity):
+        return self.entity2id[entity]
 
-	def entityMentioned(self, entity):
-		if self.validEntity(entity):
-			eid = '{}'.format(self.eidFinder(entity))
-			return self.inverted_index.loc[self.inverted_index['eid'] == eid].mid.tolist()
-		else:
-			return False
+    def entityMentioned(self, entity):
+        if self.validEntity(entity):
+            eid = '{}'.format(self.eidFinder(entity))
+            return self.inverted_index.loc[self.inverted_index['eid'] == eid].mid.tolist()
+        else:
+            return False
 
-	def key2Text(self, mentionedKeys):
-		mentionedSet = set(mentionedKeys)
-		content = []
+    def key2Text(self, mentionedKeys):
+        mentionedSet = set(mentionedKeys)
+        content = []
 
-		for key in mentionedSet:
-			rec = '|'.join(self.sent_index.loc[self.sent_index['mid'] == key].values[0].tolist())
-			content.append(rec)
+        for key in mentionedSet:
+            rec = '|'.join(self.sent_index.loc[self.sent_index['mid'] == key].values[0].tolist())
+            content.append(rec)
 
-		return content
+        return content
+
+    def parseMentions(self, mlist):
+        mentions = {}
+        for rec in mlist:
+            part = rec.split('-')
+            if part[0] not in mentions.keys():
+                mentions.update({part[0]:{part[1]:set(part[2])}})
+            elif part[1] not in mentions[part[0]].keys():
+                mentions[part[0]].update({part[1]:set(part[2])})
+            else:
+                mentions[part[0]][part[1]].add(part[2])
+        return mentions
