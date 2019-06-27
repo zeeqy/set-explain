a3122bba0b320f1b499ae9ad9115ef907ab96407
Zhiqi Huang <Charles.hzq@gmail.com>
2019-06-27 10:30:18 -0400

    json clean

--- processing/pos_sentence.py
+++ processing/pos_sentence.py
@@ -2,7 +2,7 @@ import json, sys, os, re
 import argparse
 import bisect
 import spacy
-nlp = spacy.load("en_core_web_sm")
+nlp = spacy.load("en_core_web_sm", disable=['ner'])
 import threading
 
 """
