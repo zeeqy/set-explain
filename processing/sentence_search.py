bfde48db0155ef143e72b0aaf8d4a5546f23e8d9
Zhiqi Huang <Charles.hzq@gmail.com>
2019-07-06 17:20:26 -0400

    sentence search

--- processing/sentence_search.py
+++ processing/sentence_search.py
@@ -22,7 +22,7 @@ def merge_task(task_list, args):
 
         for item in tqdm(doc, desc='{}'.format(fname), mininterval=30):
             item_dict = json.loads(item)
-            entity_text = set([em['text'] for em in item_dict['entityMentions']])
+            entity_text = set([em['text'] for em in item_dict['np_chunks']])
             if entity_text.intersection(keywords) == keywords:
                 context.append(item_dict['tokens'])
         
