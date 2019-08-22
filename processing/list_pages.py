8c0044fd72d914c3615a60de4a71d061757f022d
zeeqy <hgzhiqi@gmail.com>
2019-08-22 01:43:20 -0400

    list pages

--- processing/list_pages.py
+++ processing/list_pages.py
@@ -1,37 +1,60 @@
-import json, sys, os, re
+import json, sys, os, re, time
 import argparse
 import bisect
 import multiprocessing as mp
 from tqdm import tqdm
 from bs4 import BeautifulSoup
+from wikitables import import_tables
 
 """
 keep json format and find list pages
 
 """
 
-def merge_task(task_list, args):
-	for folder in task_list:
-		outputname = 'XML_{}'.format(folder)
-		working_dir = '{}/{}'.format(args.input_dir,folder)
-		context = []
-		for fname in os.listdir(working_dir):
-			with open('{}/{}'.format(working_dir,fname), 'r') as f:
-				raw = f.read()
-			f.close()
-			soup = BeautifulSoup(raw,'html')
-			docs = soup.find_all('doc')
-			for doc in tqdm(docs, desc='{}'.format(fname), mininterval=30):
-				if doc['title'][:7] != "List of":
-					continue # filter lists
-				else:
-					context.append(doc)
+def merge_task(task_list, args, pid):
+	context = []
+	outputname = 'LIST-{}'.format(pid)
+	for wt in tqdm(task_list, desc='pages-{}'.format(pid), mininterval=30):
+		tables = import_tables(wt)
+		if len(tables) > 1:
+			title = parse(wt[8:].lower())
+			if title != '':
+				ent_list = []
+				for row in tables[0].rows:
+					key = list(row.keys())[0]
+					ent = parse(row[key].lower())
+					if ent != '':
+						ent_list.append(ent)
+				if len(ent_list) >= 5:
+					context.append(context.append({'title':title, 'ents':ent_list}))
+		time.sleep(0.1)
+	
+	if context != []:
+	with open('{}/{}'.format(args.output_dir, outputname), "w+") as f:
+		f.write('\n'.join(context))
+	f.close()
 
-		if context != []:
-			with open('{}/{}'.format(args.output_dir, outputname), "w+") as f:
-				for doc in context:
-					f.write(str(doc))
-			f.close()
+	# for folder in task_list:
+	# 	outputname = 'XML_{}'.format(folder)
+	# 	working_dir = '{}/{}'.format(args.input_dir,folder)
+	# 	context = []
+	# 	for fname in os.listdir(working_dir):
+	# 		with open('{}/{}'.format(working_dir,fname), 'r') as f:
+	# 			raw = f.read()
+	# 		f.close()
+	# 		soup = BeautifulSoup(raw,'html')
+	# 		docs = soup.find_all('doc')
+	# 		for doc in tqdm(docs, desc='{}'.format(fname), mininterval=30):
+	# 			if doc['title'][:7] != "List of":
+	# 				continue # filter lists
+	# 			else:
+	# 				context.append(doc)
+
+	# 	if context != []:
+	# 		with open('{}/{}'.format(args.output_dir, outputname), "w+") as f:
+	# 			for doc in context:
+	# 				f.write(str(doc))
+	# 		f.close()
 
 def split(a, n):
 	k, m = divmod(len(a), n)
@@ -71,16 +94,20 @@ def parse(text):
 
 def main():
 	parser = argparse.ArgumentParser(description="keep json format and find list pages")
-	parser.add_argument('--input_dir', type=str, default='', help='dump file directory')
+	parser.add_argument('--list_pages', type=str, default='', help='list pages')
 	parser.add_argument('--output_dir', type=str, default='', help='output directory')
 	parser.add_argument('--num_process', type=int, default=2, help='number of parallel')
 	
 	args = parser.parse_args()
 
-	dump_dir = os.listdir(args.input_dir)
-	tasks = list(split(dump_dir, args.num_process))
+	with open(args.list_pages, 'r') as f:
+		list_pages = f.read()
+	f.close()
 
-	processes = [mp.Process(target=merge_task, args=(tasks[i], args)) for i in range(args.num_process)]
+	list_pages = list(set(list_pages.split('\n')))[:100]
+	tasks = list(split(list_pages, args.num_process))
+
+	processes = [mp.Process(target=merge_task, args=(tasks[i], args, i)) for i in range(args.num_process)]
 
 	for p in processes:
 		p.start()
