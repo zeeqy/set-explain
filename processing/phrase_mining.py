6175b9630406d4577acb1002cb0c16329faa8942
zeeqy <hgzhiqi@gmail.com>
2019-09-11 17:49:27 -0400

    add

--- processing/phrase_mining.py
+++ processing/phrase_mining.py
@@ -255,7 +255,7 @@ def main():
         score = 0
         tokens = nltk.word_tokenize(phrase)
         nonstop_tokens = [token for token in tokens if token not in stop]
-        if len(nonstop_tokens) / len(token) <= 0.5:
+        if len(nonstop_tokens) / len(tokens) <= 0.5:
             continue
         raw_tokenized = tokenizer.tokenize(tokens)
         tokenized_set = set(raw_tokenized)
