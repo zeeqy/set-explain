0e480fde3d97f7153417a8ed86691228add23e2a
zeeqy <hgzhiqi@gmail.com>
2019-09-23 16:53:57 -0400

    phrase

--- processing/heuristic-app.py
+++ processing/heuristic-app.py
@@ -218,7 +218,7 @@ def main_thrd(query, num_process, input_dir, target):
             elif token in unistems:
                 phrase_vec.append(0)
 
-        phrases_score.update({phrase:{'score': score/len(nonstop_tokens), 'tfidf_sim': np.dot(target_vec, phrase_vec)/(norm(target_vec) * norm(phrase_vec))}})
+        phrases_score.update({phrase:{'score': score/len(nonstop_tokens), 'tfidf_sim': np.dot(target_vec, phrase_vec)/(np.linalg.norm(target_vec) * np.linalg.norm(phrase_vec))}})
 
     phrases_sorted = sorted(phrases_score.items(), key=lambda x: x[1]['score'], reverse=True)
 
