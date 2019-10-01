4cb9eb3df9d8838094604d61e2b40c205d0cd2fa
zeeqy <hgzhiqi@gmail.com>
2019-10-01 19:52:29 -0400

    table scraper

--- processing/table_scraper.py
+++ processing/table_scraper.py
@@ -1,8 +1,7 @@
-from aiohttp import ClientSession, TCPConnector
-import asyncio
+import multiprocessing as mp
+from multiprocessing import Pool
 import sys, os, time
-from pypeln import asyncio_task as aio
-import urllib
+import request
 import argparse
 from tqdm import tqdm
 import json
@@ -11,90 +10,70 @@ import json
 import time
 from bs4 import BeautifulSoup
 
-entityset = set()
-table_collect = []
+def scraper(params):
+    wikititle, entityset, pid = params
 
-async def fetch(t, session):
-	global table_collect
-	url = 'https://en.wikipedia.org/wiki/{}'.format(t.replace(' ', '_'))
-	title_text = t[8:]
-	async with session.get(url) as response:
-		res = await response.read()
-		soup = BeautifulSoup(res,'lxml')
-		_table = soup.find('table',{'class':'wikitable'})
-		if _table is None or _table.tbody is None:
-			return
-		ent = []
-		for row in _table.tbody.findAll('tr')[1:]:
-			if len(row.findAll('td')) == 0:
-				continue
-			first_column = row.findAll('td')[0]
-			try:
-				string = first_column.find('a').text
-			except:
-				continue
-			if string is None:
-				continue
-			if '(page does not exist)' in string:
-				continue
-			else:
-				string = re.sub(r'\([^)]*\)', '', string)
-				try: 
-					string = string.encode('ascii').decode()
-				except:
-					continue
-				string = string.lower()
-				if string.split(',')[0] in entityset:
-					ent.append(string.split(',')[0])
-		if len(ent) >= 10:
-			tid = np.random.randint(10000, high=99999, size=None, dtype='int64')
-			table_collect.append({'id':'WL{}'.format(tid), 'title':title_text, "entities":ent, 'url':url})
+    tables = []
+    tid = 1
+    for title in tqdm(wikititle, desc='processing-{}'.format(pid), mininterval=10):
+        url = 'https://en.wikipedia.org/wiki/{}'.format(title.replace(' ', '_'))
+        website_url = requests.get(url).text
+        title_text = title[8:]
+        soup = BeautifulSoup(website_url,'lxml')
+        My_table = soup.find('table',{'class':'wikitable'})
+        if My_table is None or My_table.tbody is None:
+            continue
+        ent = []
+        for row in My_table.tbody.findAll('tr')[1:]:
+            if len(row.findAll('td')) == 0:
+                continue
+            first_column = row.findAll('td')[0]
+            try:
+                string = first_column.find('a').text
+            except:
+                continue
+            if string is None:
+                continue
+            if '(page does not exist)' in string:
+                continue
+            else:
+                string = re.sub(r'\([^)]*\)', '', string)
+                try: 
+                    string = string.encode('ascii').decode()
+                except:
+                    continue
+                string = string.lower()
+                if string.split(',')[0] in entityset:
+                    ent.append(string.split(',')[0])
+        if len(ent) >= 10:
+            tables.append({'id':'SCR{}{}'.format(pid,tid), 'title':title_text, "entities":ent, 'url':url})
+            tid +=1
+        
+    if len(tables) != 0:
+    with open('/mnt/nfs/work1/allan/zhiqihuang/set-explain/data/SCRAPER_{}'.format(pid+1), 'w+') as f:
+        for s in tables:
+            f.write(json.dumps(s)+'\n')
+    f.close()
 
-def main():
-	global table_collect
-	parser = argparse.ArgumentParser(description="Page table scraper")
-	parser.add_argument('--entity_dir', type=str, default='', help='dump file directory')
-	parser.add_argument('--title_dir', type=str, default='', help='dump file directory')
-	parser.add_argument('--output_dir', type=str, default='', help='output directory')
-	args = parser.parse_args()
 
-	with open('{}/wiki_quality.txt'.format(args.entity_dir), 'r') as f:
-		raw_list = f.read()
-	f.close()
-	entityset = set(raw_list.split('\n'))
+def split(a, n):
+    k, m = divmod(len(a), n)
+    return (a[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(n))
 
-	with open('{}/list_pages.txt'.format(args.title_dir), 'r') as f:
-		raw_list = f.read()
-	f.close()
-	wikititle = raw_list.split('\n')
+with open('/mnt/nfs/work1/allan/zhiqihuang/HiExpan/src/tools/AutoPhrase/data/EN/wiki_quality.txt', 'r') as f:
+    raw_list = f.read()
+f.close()
+entityset = set(raw_list.split('\n'))
 
-	limit = 80
+with open('/mnt/nfs/work1/allan/zhiqihuang/set-explain/data/list_pages.txt', 'r') as f:
+    raw_list = f.read()
+f.close()
 
-	chunks = [wikititle[i:i + 80] for i in range(0, len(wikititle), 80)]
-	i = 1
-	for c in tqdm(chunks, desc='processing-chunks', mininterval=10):
-		aio.each(
-			fetch, 
-			c,
-			workers = limit,
-			on_start = lambda: ClientSession(connector=TCPConnector(limit=None)),
-			on_done = lambda _status, session: session.close(),
-			run = True,
-		)
-		time.sleep(1)
+wikititle = raw_list.split('\n')
 
-		if len(table_collect) != 0:
+tasks = list(split(wikititle, 10))
 
-			with open('{}/SCRAPER_{}'.format(args.output_dir,i), 'w+') as f:
-				for s in table_collect:
-					f.write(json.dumps(s)+'\n')
-			f.close()
-			i += 1
-		
-		table_collect = []
-		
-	return 0
+inputs = [(tasks[i], entityset, i) for i in range(num_process)]
 
-if __name__ == '__main__':
-	main()
-	
+with Pool(10) as p:
+    p.map(scraper, inputs)
