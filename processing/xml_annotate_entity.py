00cb9c408a295cf8cf36fc6ee31722e1f5c40e5d
zeeqy <hgzhiqi@gmail.com>
2019-10-06 22:10:27 -0400

    add more doc

--- processing/xml_annotate_entity.py
+++ processing/xml_annotate_entity.py
@@ -7,6 +7,16 @@ import string
 from nltk.tokenize import MWETokenizer
 import nltk
 
+pronoun = set(["he", "her", "hers", "herself", "him", "himself", "his", "i", 
+"it", "its", "me", "mine", "my", "myself", "our", "ours", "ourselves", 
+"she", "thee", "their", "them", "themselves", "they", "thou", 
+"thy", "thyself", "us", "we", "ye", "you", "your", "yours", "yourself",
+"we", "anybody", "anyone", "anything", "each", "either", "everybody", 
+"everyone", "everything", "neither", "nobody", "no one", "nothing", "one", "somebody", 
+"someone", "something", "all", "any", "most", "none", "some", "both", "few", "many", "several",
+ "what", "who", "which", "whom", "whose", "that", "whichever", "whoever", "whomever", 
+ "this", "these", "that", "those"])
+
 """
 find entities mentioned in each sentence
 
@@ -37,12 +47,15 @@ def merge_task(task_list, args):
 
         for item in tqdm(doc, desc='{}'.format(fname), mininterval=30):
             item_dict = json.loads(item)
+            if set(item_dict['nsubj']).issubset(pronoun):
+                continue
             sent = nltk.word_tokenize(item_dict['text'])
             raw_tokenized = tokenizer.tokenize(sent)
             tokenized_set = set(raw_tokenized)
             mentioned_entity = list(tokenized_set.intersection(entityset))
             if len(mentioned_entity) != 0:
                 item_dict.update({'entityMentioned':mentioned_entity})
+                item_dict['iid'] = '{}{}{}'.format(item_dict['did'],item_dict['pid'],item_dict['sid'])
                 context.append(json.dumps(item_dict))
 
         with open('{}/{}'.format(args.output_dir, outputname), "w+") as f:
